1d5fad91e83bf4e8ea05e4336535deafa93d39dd
Bruno Brandini <55330738+Bruknown@users.noreply.github.com>
2025-09-11 15:25:25 -0300

    Migration to New Project Idea

---
+++ src/test/java/com/brunosoft/produtosAPI/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosoft.produtosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
