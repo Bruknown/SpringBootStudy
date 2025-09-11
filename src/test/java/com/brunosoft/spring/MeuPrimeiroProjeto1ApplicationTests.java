1bbcba9078ed679c76d64bc27bbcfdb595974603
Bruno Brandini <55330738+Bruknown@users.noreply.github.com>
2025-09-11 02:52:50 -0300

    UpdatedProject

--- src/test/java/com/brunosoft/spring/MeuPrimeiroProjeto1ApplicationTests.java
+++ src/test/java/com/brunosoft/spring/MeuPrimeiroProjeto1ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MeuPrimeiroProjetoApplicationTests {
+class MeuPrimeiroProjeto1ApplicationTests {
 
 	@Test
 	void contextLoads() {
